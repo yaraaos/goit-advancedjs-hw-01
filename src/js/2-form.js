18f7a78c5da92ffbf485dbadc1a5268e86ea25a8
yaraaos <yara144ss@gmail.com>
2024-12-09 23:10:56 +0200

    deploy

---
+++ src/js/2-form.js
@@ -0,0 +1,35 @@
+let formData = {
+    email: "",
+    message: "",
+};
+
+const savedData = localStorage.getItem('feedback-form-state');
+if (savedData) {
+  formData = JSON.parse(savedData);
+}
+
+const formEl = document.querySelector('.feedback-form');
+const emailEl = formEl.elements.email;
+const messageEl = formEl.elements.message;
+
+emailEl.value = formData.email;
+messageEl.value = formData.message;
+
+formEl.addEventListener('input', event => {
+    formData[event.target.name] = event.target.value.trim();
+    localStorage.setItem('feedabck-form-state', JSON.stringify(formData));
+});
+
+formEl.addEventListener('submit', event => {
+    event.preventDefault();
+
+    if (!formData.email || !formData.message){
+        alert ('Fill please all fields');
+        return;
+    }
+
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    formData = { email: '', message: '' };
+    formEl.reset();
+});
