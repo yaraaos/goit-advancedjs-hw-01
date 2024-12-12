19a44bbe453e8dbd9c183a8929631127a590f755
yaraaos <yara144ss@gmail.com>
2024-12-12 23:41:01 +0200

    ?

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,17 +3,16 @@ let formData = {
     message: "",
 };
 
-const savedData = localStorage.getItem('feedback-form-state');
-if (savedData) {
-  formData = JSON.parse(savedData);
-}
-
 const formEl = document.querySelector('.feedback-form');
 const emailEl = formEl.elements.email;
 const messageEl = formEl.elements.message;
 
-emailEl.value = formData.email;
-messageEl.value = formData.message;
+const savedData = localStorage.getItem('feedback-form-state');
+if (savedData) {
+  formData = JSON.parse(savedData);
+  emailEl.value = formData.email || '';
+  messageEl.value = formData.message || '';
+}
 
 formEl.addEventListener('input', event => {
     formData[event.target.name] = event.target.value.trim();
@@ -29,6 +28,7 @@ formEl.addEventListener('submit', event => {
     }
 
     console.log(formData);
+    
     localStorage.removeItem('feedback-form-state');
     formData = { email: '', message: '' };
     formEl.reset();
